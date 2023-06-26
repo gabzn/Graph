59cb0c2a4e5aed479dca9af028651a288a72a538
Gabriel <58453421+gabzn@users.noreply.github.com>
2023-06-25 21:54:46 -0400

    Update Postorder-dfs-iterative.py

--- Postorder-dfs-iterative.py
+++ Postorder-dfs-iterative.py
@@ -13,23 +13,27 @@ def postorder_dfs(graph):
     visited_nodes = set()
 
     for node in graph:
-        if node not in visited_nodes:
-            dfs_stack.append(node)
+        if node in visited_nodes:
+            continue
 
-            postorder = []
-            while dfs_stack:
-                cur_node = dfs_stack.pop()
-                
-                if cur_node in visited_nodes:
-                    continue
-                visited_nodes.add(cur_node)
+        # Visit all nodes in preorder fashion first.
+        preorder = []
+        dfs_stack.append(node)
+        while dfs_stack:
+            cur_node = dfs_stack.pop()
+            
+            if cur_node in visited_nodes:
+                continue
+            visited_nodes.add(cur_node)
+            
+            preorder.append(cur_node)
+            
+            for neighbour in graph[cur_node]:
+                dfs_stack.append(neighbour)
 
-                postorder.append(cur_node)
-                
-                for neighbour in graph[cur_node]:
-                    dfs_stack.append(neighbour)
-
-            while postorder:
-                ordering.append(postorder.pop())
+        # After visiting all the neighbours in preorder fashion, we start popping nodes from the end 
+        # to simulate that we are visiting all the neighbours first.
+        while preorder:
+            ordering.append(preorder.pop())
 
     return ordering
